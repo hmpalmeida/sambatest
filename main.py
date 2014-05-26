f8eab059897c628ec67398b705eab2d4ea54ea0d
Helio <helio@zebes.(none)>
2014-05-26 18:50:38 -0300

    Video conversion partially added
    Functionality for video conversion added to main.py.
    
    TODO:
      - For the moment, we assume that the job submission will work.
         This has to change.
      - We also assume that the file will be immediately available
         in the S3 storage, which is not true.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cgi
 import urllib
+import urllib2
 import os
+import json
 
 from google.appengine.ext import ndb
 
@@ -37,10 +39,6 @@ class Video(ndb.Model):
      name = ndb.StringProperty(indexed=False)
      date = ndb.DateTimeProperty(auto_now_add=True)
      
-     #@classmethod
-     #def query_url(cls, ancestor_key):
-     #     return cls.query(ancestor=ancestor_key).order(-cls.date)
-     
 
 #####################################################
 # Main class that will work on all requests
@@ -49,23 +47,30 @@ class Video(ndb.Model):
 class MainPage(webapp2.RequestHandler):
 
      def get(self):
+          # TESTING ONLY
+          #queries = Video.query()
+          #for q in queries:
+          #     print q
+          # END TESTING
           video_url = self.request.get('url', DEFAULT_VIDEO).strip()
           video_name = self.request.get('name', DEFAULT_VIDEO).strip()
+          print video_name
           if (video_url == DEFAULT_VIDEO):
                # If the default video isn't yet on the Store,
                # than store it
+               tmp = video_name.rpartition('/')
+               video_name = tmp[2]
                video_query = Video.query(Video.url == video_url)
                if (video_query.count() == 0):
-                    video_name = video_name.rpartition('/')[2]
                     v = Video(parent=video_key())
                     v.url = video_url
                     v.name = video_name
                     v.put()
-          
+          # Prepare template parameters
           template_values = {
                'file': SERVER_NAME+video_name,
           }
-
+          # Call the template
           template = JINJA_ENVIRONMENT.get_template('index.html')
           self.response.write(template.render(template_values))
 
@@ -75,12 +80,24 @@ class MainPage(webapp2.RequestHandler):
           # Get the video name from the url
           tmp = v.url.rpartition('/')
           v.name = tmp[2]
+          # Create name for output file
+          tmp = v.name.rpartition('.')
+          v.name = tmp[0]+'.mp4'
           # Check if this file was already processed
           video_query = Video.query(Video.url == v.url)
-          #video_query = Video.query(ancestor=video_key()).order(-Greeting.date)
           if (video_query.count() == 0):
-               # TODO Convert new video to a suitable format
-               # Add the new video to the store
+               # Convert new video to a suitable format
+               zenc_input = {'input' : v.url, 'output' : {'url': 's3://sambatest/'+v.name, 'public': 'true'} } 
+               enc_input = json.dumps(zenc_input)
+               header = {'Zencoder-Api-Key': '93d453aa7de28c6645a88b5df3dd59a1', 'Content-Type': 'application/json'}
+               req = urllib2.Request('https://app.zencoder.com/api/v2/jobs', enc_input, header)
+               try:
+                    f = urllib2.urlopen(req)
+               except urllib2.HTTPError as e:
+                    print e.code
+                    print e.read()
+               print f
+               # If everything went ok, add the new video to the store
                v.put()
           # Redirect to the "/" get path to open the desired file
           query_params = {'name': v.name , 'url': v.url }

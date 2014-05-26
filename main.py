4e5a7982df83f79a5a502c8b56d05ec01876be44
Helio <helio@zebes.(none)>
2014-05-26 14:06:27 -0300

    Code cleaning in main.py
    Streamlining and code cleaning in functions get and post.

--- main.py
+++ main.py
@@ -49,21 +49,20 @@ class Video(ndb.Model):
 class MainPage(webapp2.RequestHandler):
 
      def get(self):
-          video_url = self.request.get('video_name', DEFAULT_VIDEO).strip()
-          tmp = video_url.rpartition('/')
-          video_name = tmp[2]
+          video_url = self.request.get('url', DEFAULT_VIDEO).strip()
+          video_name = self.request.get('name', DEFAULT_VIDEO).strip()
           if (video_url == DEFAULT_VIDEO):
                # If the default video isn't yet on the Store,
                # than store it
                video_query = Video.query(Video.url == video_url)
                if (video_query.count() == 0):
+                    video_name = video_name.rpartition('/')[2]
                     v = Video(parent=video_key())
                     v.url = video_url
                     v.name = video_name
                     v.put()
           
           template_values = {
-               #'server': SERVER_NAME,
                'file': SERVER_NAME+video_name,
           }
 
@@ -71,22 +70,20 @@ class MainPage(webapp2.RequestHandler):
           self.response.write(template.render(template_values))
 
      def post(self):
-          new_video = self.request.get('video_url').strip()
+          v = Video(parent=video_key())
+          v.url = self.request.get('video_url').strip()
+          # Get the video name from the url
+          tmp = v.url.rpartition('/')
+          v.name = tmp[2]
           # Check if this file was already processed
-          video_query = Video.query(Video.url == new_video)
+          video_query = Video.query(Video.url == v.url)
           #video_query = Video.query(ancestor=video_key()).order(-Greeting.date)
           if (video_query.count() == 0):
                # TODO Convert new video to a suitable format
                # Add the new video to the store
-               v = Video(parent=video_key())
-               v.url = new_video
-               # Get the video name from the url
-               tmp = v.url.rpartition('/')
-               v.name = tmp[2]
                v.put()
-          #else:
           # Redirect to the "/" get path to open the desired file
-          query_params = {'video_name': new_video}
+          query_params = {'name': v.name , 'url': v.url }
           self.redirect('/?' + urllib.urlencode(query_params))
 
 #####################################################

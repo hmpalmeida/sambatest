55fb6a21356f5ba7d64c0d4defc78c3aaf425e77
Helio <helio@zebes.(none)>
2014-05-28 19:23:18 -0300

    Changed ndb checking for S3 checking
    The ndb based schema for checking if a video had already been
    encoded was not working as intended. Ndb usage was removed. We
    now check for the file in the S3 storage using boto.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import urllib2
 import os
 import json
 import time
-
-from google.appengine.ext import ndb
+import boto
+import boto.s3.connection
 
 import webapp2
 import jinja2
@@ -21,27 +21,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 SERVER_NAME = 'http://d357rxfqtl15q4.cloudfront.net/'
 
-#DEFAULT_VIDEO = 'dramatic.mp4'
 DEFAULT_VIDEO = 'file:///home/helio/Videos/dramatic.mp4'
 
 DEFAULT_STORE = 'videolist'
 
 ZENCODER_KEY = '93d453aa7de28c6645a88b5df3dd59a1'
 
-#####################################################
-# Classes and definitions for the ndb store
-######################################################
-
-def video_key():
-     """Constructs a Datastore key for a Videos entity within DEFAULT_STORE."""
-     return ndb.Key('Videos', DEFAULT_STORE)
-
-class Video(ndb.Model):
-     """Stores information on a video."""
-     url = ndb.StringProperty()
-     name = ndb.StringProperty(indexed=False)
-     date = ndb.DateTimeProperty(auto_now_add=True)
-     
 #####################################################
 # Helper functions to create Zencoder requests
 #####################################################
@@ -56,6 +41,19 @@ def create_zencoder_job_request(input_url, output_file):
      req = urllib2.Request('https://app.zencoder.com/api/v2/jobs', 
           enc_input, header)
      return req
+
+def in_s3(filename):
+     # Open connection to s3
+     conn = boto.connect_s3(aws_access_key_id = 'AKIAI4OB44FR6GURVEXA',
+               aws_secret_access_key='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'
+     )
+     # Get Bucket
+     mybucket = conn.get_bucket('sambatest')
+     if (filename in mybucket):
+          return True
+     else:
+          return False
+
 
 #####################################################
 # Main class that will work on all requests
@@ -110,12 +108,6 @@ class MainPage(webapp2.RequestHandler):
                # than store it
                tmp = video_name.rpartition('/')
                video_name = tmp[2]
-               video_query = Video.query(Video.url == video_url)
-               if (video_query.count() == 0):
-                    v = Video(parent=video_key())
-                    v.url = video_url
-                    v.name = video_name
-                    v.put()
           # Prepare template parameters
           template_values = {
                'file': SERVER_NAME+video_name,
@@ -131,20 +123,17 @@ class MainPage(webapp2.RequestHandler):
      ###################
 
      def post(self):
-          v = Video(parent=video_key())
-          v.url = self.request.get('video_url').strip()
+          video_url = self.request.get('video_url').strip()
           # Get the video name from the url
-          tmp = v.url.rpartition('/')
-          v.name = tmp[2]
+          tmp = video_url.rpartition('/')
+          video_name = tmp[2]
           # Create name for output file
-          tmp = v.name.rpartition('.')
-          v.name = tmp[0]+'.mp4'
+          tmp = video_name.rpartition('.')
+          video_name = tmp[0]+'.mp4'
           # Check if this file was already processed
-          video_query = Video.query(Video.url == v.url)
-          #FIXME this checking is useless at the moment!
-          if (video_query.count() >= 0):
+          if (not in_s3(video_name)):
                # Convert new video to a suitable format
-               req = create_zencoder_job_request(v.url, v.name)
+               req = create_zencoder_job_request(video_url, video_name)
                try:
                     f = urllib2.urlopen(req)
                except urllib2.HTTPError as e:
@@ -154,12 +143,12 @@ class MainPage(webapp2.RequestHandler):
                     return
                # Request submitted, now control job status
                response = json.loads(f.read())
-               query_params = {'name': v.name , 'url': v.url,
+               query_params = {'name': video_name , 'url': video_url,
                      'jobid': str(response['id']) }
                self.redirect('/?' + urllib.urlencode(query_params))
                return
           # Redirect to the "/" get path to open the desired file
-          query_params = {'name': v.name , 'url': v.url }
+          query_params = {'name': video_name , 'url': video_url }
           self.redirect('/?' + urllib.urlencode(query_params))
 
 #####################################################
